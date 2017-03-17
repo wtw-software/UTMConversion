a453ded16f8eaedcd881dea0c68872ce7ec9324d
Peter Ringset <peterringset@wtw.no>
2017-03-17 13:00:49 +0100

    Added project structure

---
+++ UTMConversion/UTMConversion.h
@@ -0,0 +1,19 @@
+//
+//  UTMConversion.h
+//  UTMConversion
+//
+//  Created by Peter Ringset on 17/03/2017.
+//  Copyright © 2017 Peter Ringset. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UTMConversion.
+FOUNDATION_EXPORT double UTMConversionVersionNumber;
+
+//! Project version string for UTMConversion.
+FOUNDATION_EXPORT const unsigned char UTMConversionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UTMConversion/PublicHeader.h>
+
+
