ece17b5ae2f2aab5458107e0e798c3e8f239dbc2
Huw Rowlands <hrowlands@dius.com.au>
2017-08-26 12:59:57 +1000

    Add test for Buninyong
    src: http://www.ga.gov.au/bin/geodesy/run/gda_redfearn?geo_grid=1&Name=&lat_degrees=-37&lat_minutes=39&lat_seconds=10.15611&lon_degrees=143&lon_minutes=55&lon_seconds=35.38393&Submit=Submit+Data

--- UTMConversionTests/UTMConversionTests.swift
+++ UTMConversionTests/UTMConversionTests.swift
@@ -30,28 +30,40 @@ class UTMConversionTests: XCTestCase {
         XCTAssertEqualWithAccuracy(johannesburgUTM.easting, 603914, accuracy: 0.00001)
         XCTAssertEqual(johannesburgUTM.zone, 35)
         XCTAssertEqual(johannesburgUTM.hemisphere, .southern)
+
+        let buninyongUTM = buninyong.utmCoordinate()
+        XCTAssertEqualWithAccuracy(buninyongUTM.northing, 5828674.33994, accuracy: 0.00001)
+        XCTAssertEqualWithAccuracy(buninyongUTM.easting, 758173.79835, accuracy: 0.00001)
+        XCTAssertEqual(buninyongUTM.zone, 54)
+        XCTAssertEqual(buninyongUTM.hemisphere, .southern)
     }
-    
+
     func testCLLocation_utmCoordinate() {
         let osloUTM = osloLocation.utmCoordinate()
         XCTAssertEqualWithAccuracy(osloUTM.northing, 6643010.0, accuracy: 0.00001);
         XCTAssertEqualWithAccuracy(osloUTM.easting, 598430.0, accuracy: 0.00001);
         XCTAssertEqual(osloUTM.zone, 32)
         XCTAssertEqual(osloUTM.hemisphere, .northern)
-        
+
         let trondheimUTM = trondheimLocation.utmCoordinate()
         XCTAssertEqualWithAccuracy(trondheimUTM.northing, 7034313, accuracy: 0.00001)
         XCTAssertEqualWithAccuracy(trondheimUTM.easting, 569612, accuracy: 0.00001)
         XCTAssertEqual(trondheimUTM.zone, 32)
         XCTAssertEqual(trondheimUTM.hemisphere, .northern)
-        
+
         let johannesburgUTM = johannesburgLocation.utmCoordinate()
         XCTAssertEqualWithAccuracy(johannesburgUTM.northing, 7100115, accuracy: 0.00001)
         XCTAssertEqualWithAccuracy(johannesburgUTM.easting, 603914, accuracy: 0.00001)
         XCTAssertEqual(johannesburgUTM.zone, 35)
         XCTAssertEqual(johannesburgUTM.hemisphere, .southern)
+
+        let buninyongUTM = buninyongLocation.utmCoordinate()
+        XCTAssertEqualWithAccuracy(buninyongUTM.northing, 5828674.33994, accuracy: 0.00001)
+        XCTAssertEqualWithAccuracy(buninyongUTM.easting, 758173.79835, accuracy: 0.00001)
+        XCTAssertEqual(buninyongUTM.zone, 54)
+        XCTAssertEqual(buninyongUTM.hemisphere, .southern)
     }
-    
+
     func testUTMCoordinate_coordinate() {
         let oslo = osloUTM.coordinate()
         XCTAssertEqual(oslo.latitude, 59.912814611065265)
@@ -60,11 +72,14 @@ class UTMConversionTests: XCTestCase {
         let trondheim = trondheimUTM.coordinate()
         XCTAssertEqual(trondheim.latitude, 63.430493678423012)
         XCTAssertEqual(trondheim.longitude, 10.394966844991798)
-        
-        let utmJohannesburg = UTMCoordinate(northing: 7100115, easting: 603914, zone: 35, hemisphere: .southern)
-        let johannesburg = utmJohannesburg.coordinate()
+
+        let johannesburg = johannesburgUTM.coordinate()
         XCTAssertEqual(johannesburg.latitude, -26.214767103043133)
         XCTAssertEqual(johannesburg.longitude, 28.040197220939884)
+
+        let buninyong = buninyongUTM.coordinate()
+        XCTAssertEqualWithAccuracy(buninyong.latitude, -37.65282114, accuracy: 0.00001)
+        XCTAssertEqualWithAccuracy(buninyong.longitude, 143.92649554, accuracy: 0.00001)
     }
     
     func testUTMCoordinate_location() {
@@ -79,19 +94,26 @@ class UTMConversionTests: XCTestCase {
         let johannesburg = johannesburgUTM.location()
         XCTAssertEqual(johannesburg.coordinate.latitude, -26.214767103043133)
         XCTAssertEqual(johannesburg.coordinate.longitude, 28.040197220939884)
+
+        let buninyong = buninyongUTM.location()
+        XCTAssertEqualWithAccuracy(buninyong.coordinate.latitude, -37.65282114, accuracy: 0.00001)
+        XCTAssertEqualWithAccuracy(buninyong.coordinate.longitude, 143.92649554, accuracy: 0.00001)
     }
     
 }
 
 
+private let buninyong = CLLocationCoordinate2D(latitude: -37.65282114, longitude: 143.92649554)
 private let oslo = CLLocationCoordinate2D(latitude: 59.912814611065265, longitude: 10.760192985178369)
 private let trondheim = CLLocationCoordinate2D(latitude: 63.430493678423012, longitude: 10.394966844991798)
 private let johannesburg = CLLocationCoordinate2D(latitude: -26.214767103043133, longitude: 28.040197220939884)
 
+private let buninyongUTM = UTMCoordinate(northing: 5828674.33994, easting: 758173.79835, zone: 54, hemisphere: .southern)
 private let osloUTM = UTMCoordinate(northing: 6643010, easting: 598430, zone: 32, hemisphere: .northern)
 private let trondheimUTM = UTMCoordinate(northing: 7034313, easting: 569612, zone: 32, hemisphere: .northern)
 private let johannesburgUTM = UTMCoordinate(northing: 7100115, easting: 603914, zone: 35, hemisphere: .southern)
 
-private let osloLocation = CLLocation(latitude: 59.912814611065265, longitude: 10.760192985178369)
-private let trondheimLocation = CLLocation(latitude: 63.430493678423012, longitude: 10.394966844991798)
-private let johannesburgLocation = CLLocation(latitude: -26.214767103043133, longitude: 28.040197220939884)
+private let buninyongLocation = CLLocation(latitude: buninyong.latitude, longitude: buninyong.longitude)
+private let osloLocation = CLLocation(latitude: oslo.latitude, longitude: oslo.longitude)
+private let trondheimLocation = CLLocation(latitude: trondheim.latitude, longitude: trondheim.longitude)
+private let johannesburgLocation = CLLocation(latitude: johannesburg.latitude, longitude: johannesburg.longitude)
