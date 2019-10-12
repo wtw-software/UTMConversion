dceb2ad932002996c2de8547a8039f298a36a981
Peter Ringset <peter.ringset@eggsdesign.com>
2019-10-12 11:59:49 +0200

    Change deployment targets

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "UTMConversion",
+    platforms: [
+        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         .library(
             name: "UTMConversion",
